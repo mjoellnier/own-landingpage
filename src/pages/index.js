8a0f017497cd57620e6110c599225a0b27ab7a3d
mjoellnier <other@max-wuest.de>
2019-08-31 00:01:25 +0200

    Added scroll animation library, set out initial page declaration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,41 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import ScrollAnimation from "react-animate-on-scroll";
+import "./index.css";
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div class="grid">
+      <div>
+        <ScrollAnimation animateIn="zoomIn" animateOnce={true}>
+          <h2 id="hey">Index Page!</h2>
+        </ScrollAnimation>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    <div class="grid">
+      <div>
+        <ScrollAnimation animateIn="zoomIn" animateOnce={true}>
+          <h2 id="hey">About me page!</h2>
+        </ScrollAnimation>
+      </div>
+    </div>
+    <div class="grid">
+      <div>
+        <ScrollAnimation animateIn="zoomIn" animateOnce={true}>
+          <h2 id="hey">Projects Page!</h2>
+        </ScrollAnimation>
+      </div>
+    </div>
+    <div class="grid">
+      <div>
+        <ScrollAnimation animateIn="zoomIn" animateOnce={true}>
+          <h2 id="hey">Footer/Contact/Legal Page!</h2>
+        </ScrollAnimation>
+      </div>
+    </div>
+  </>
+);
 
-export default IndexPage
+export default IndexPage;
