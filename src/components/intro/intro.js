225bd77fb2c60bdfff7c4cea4d6270deeb444798
mjoellnier <other@max-wuest.de>
2019-09-01 22:03:36 +0200

    Added mobile styling

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -4,7 +4,12 @@ import ScrollAnimation from "react-animate-on-scroll";
 export const IntroComponent = () => {
   return (
     <div class="grid">
-      <div>
+      <div className="span1">
+        <ScrollAnimation animateIn="zoomIn" animateOnce={true}>
+          <h2 id="hey">Index Page!</h2>
+        </ScrollAnimation>
+      </div>
+      <div className="span2">
         <ScrollAnimation animateIn="zoomIn" animateOnce={true}>
           <h2 id="hey">Index Page!</h2>
         </ScrollAnimation>
