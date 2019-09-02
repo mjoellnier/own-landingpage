6f56622d009bd0d2b4236fb0b33c87f4a86344e0
mjoellnier <other@max-wuest.de>
2019-09-02 06:53:17 +0200

    Style refactoring

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -4,15 +4,16 @@ import ScrollAnimation from "react-animate-on-scroll";
 export const IntroComponent = () => {
   return (
     <div class="grid">
-      <div className="span1">
+      <div id="introDiv1" className="span1">
         <ScrollAnimation animateIn="zoomIn" animateOnce={true}>
-          <h2 id="hey">Index Page!</h2>
+          <div>
+            <h2 id="hey">Hi - I am</h2>
+            <h1>Max</h1>
+          </div>
         </ScrollAnimation>
       </div>
-      <div className="span2">
-        <ScrollAnimation animateIn="zoomIn" animateOnce={true}>
-          <h2 id="hey">Index Page!</h2>
-        </ScrollAnimation>
+      <div id="introDiv2" className="span2">
+        <h2 id="hey">Index Page!</h2>
       </div>
     </div>
   );
