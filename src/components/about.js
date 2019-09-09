a27b47a17a235636dff5521ad0a6edc322ff48e2
mjoellnier <other@max-wuest.de>
2019-09-09 07:24:17 +0200

    Added wordcloud dependecy and refactored code

--- src/components/about.js
+++ src/components/about.js
@@ -1,45 +1,20 @@
-import React from "react";
-import { AnimatedOnScroll } from "react-animated-css-onscroll";
-import TextLoop from "react-text-loop";
+import React from "yurnalist/src/components/node_modules/react";
+import ReactWordcloud from "react-wordcloud";
+import words from "../constants/wordCloudWords";
 
 export const AboutComponent = () => {
   return (
-    <div class="grid">
-      <div id="about_anchor">
-        <div id="introDiv1" className="span1">
-          <div>
-            <h4>Hi! I am</h4>
-            <div id="maxWrapper">
-              <AnimatedOnScroll animationIn="zoomIn" animateOnce={true}>
-                <h1>Max</h1>
-              </AnimatedOnScroll>
-            </div>
-          </div>
-        </div>
-        <div id="introDiv2" className="span2">
-          <h3>
-            I'm a{" "}
-            <TextLoop
-              springConfig={{ stiffness: 180, damping: 8 }}
-              className="scrollWords"
-              children={[
-                "Java engineer",
-                "Webdev",
-                "Founder",
-                "Open source contributer",
-                "React dev",
-                "Photographer",
-                "Nerd",
-                "Warhammer player"
-              ]}
-            />{" "}
-            located in southern Germany.
-          </h3>
-          <h3>
-            It's nice to have you around. Take a look at my portfolio and feel
-            free to reach out for me when you think you could use my help.
-          </h3>
-        </div>
+    <div class="grid" id="about_anchor">
+      <div id="aboutDiv2" className="span1">
+        <ReactWordcloud
+          words={words}
+          options={{
+            enableTooltip: false,
+            fontSizes: [15, 60],
+            fontFamily: "impact",
+            colors: ["#264129", "#5a6346", "#913646", "#918036"]
+          }}
+        />
       </div>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,6 @@ const IndexPage = () => (
 
     <NavBarComponent>
       <AboutComponent />
-      <AboutComponent />
-      <AboutComponent />
-      <AboutComponent />
     </NavBarComponent>
   </>
 );
